d7a3f544f13d89f7e62280a78cc35d246c782fc7
gdhruv80 <gdhruv80@gmail.com>
2017-10-16 19:40:03 -0700

    Basic OOPS practice Code

---
+++ OOPS_Udacity_Media_3.py
@@ -0,0 +1,7 @@
+class Movie():
+    def __init__(self,title,storyline,image,trailer):        
+        self.title = title
+        self.storyline = storyline
+        self.image = image
+        self.trailer = trailer
+        
